7b43deec943e843ddb4822a88047567159b4e904
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-05-14 12:38:10 +0700

    Refactor to styleUrls

--- src/neo.ts
+++ src/neo.ts
@@ -4,26 +4,10 @@ import {Component} from "@angular/core";
 @Component({
   selector: "neo",
   templateUrl: "/src/neo.html",
-  styles: [
-    `
-      .editor {
-        width: 100%;
-        height: 800px;
-      }
-    `,
-    `
-      .command {
-        width: 100%;
-      }
-
-      .command > input[name=command] {
-        margin: 0;
-        padding: 0;
-        border: 0;
-        width: 100%;
-      }
-    `
-  ],
+  styleUrls: [
+    "./src/editor.neo.css",
+    "./src/command.neo.css"
+  ]
 })
 class NeoComponent { }
 
