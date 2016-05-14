f56d4a67e40e97a929f7dd2b1201afc96a692275
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-05-14 11:49:14 +0700

    Begin Neo journey

---
+++ src/neo.ts
@@ -0,0 +1,10 @@
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "neo",
+  template: "<h1>First Angular 2</h1>"
+})
+class AppComponent { }
+
+bootstrap(AppComponent);
